808d63bf5d43b9936d79c976ae4c6c25c16907c8
FuzzyLogician27 <p.ardaghwalter@hotmail.com>
2023-04-29 16:40:17 +0100

    first working commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const SOTP = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ SOTP.abilities = {
+  "str": "SOTP.AbilityStr",
+  "dex": "SOTP.AbilityDex",
+  "con": "SOTP.AbilityCon",
+  "int": "SOTP.AbilityInt",
+  "wis": "SOTP.AbilityWis",
+  "cha": "SOTP.AbilityCha"
+};
+
+SOTP.abilityAbbreviations = {
+  "str": "SOTP.AbilityStrAbbr",
+  "dex": "SOTP.AbilityDexAbbr",
+  "con": "SOTP.AbilityConAbbr",
+  "int": "SOTP.AbilityIntAbbr",
+  "wis": "SOTP.AbilityWisAbbr",
+  "cha": "SOTP.AbilityChaAbbr"
+};
